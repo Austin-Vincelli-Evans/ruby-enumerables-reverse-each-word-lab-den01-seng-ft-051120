3c139034a24bb77e08b8b4f2a761d413bb951f6f
Austin Evans <unity7591@gmail.com>
2020-05-03 20:46:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,7 @@ def reverse_each_word (string)
   new_array.each do |string|
   one_string << string.reverse!
   end
+<<<<<<< HEAD
   one_string.join(" ")
 end
 
@@ -15,4 +16,10 @@ def reverse_each_word (string)
   end
   one_string.join(" ")
 end
+=======
+  one_string.join
+end
+
+
+>>>>>>> 1eacb43b5fdecc1e8a2d63974a48158d12eac28d
 
